a4295f376e4afcaa98bfa5e75e5470a650cacec0
Dmitry <dominicbellucci04@gmail.com>
2024-08-16 20:01:25 +0300

    ну тут словами не описать, какая колоссальная работа была проделана, сижу в ахуях, что изучил так много всего об RecyclerView, больше что-то писать нет сил, чао бамбинос..

---
+++ app/src/main/java/com/mogilkin/shoppinglist/presentation/ShopListDiffCallback.kt
@@ -0,0 +1,29 @@
+package com.mogilkin.shoppinglist.presentation
+
+import androidx.recyclerview.widget.DiffUtil
+import com.mogilkin.shoppinglist.domain.ShopItem
+
+class ShopListDiffCallback(
+    private val oldList : List<ShopItem>,
+    private val newList : List<ShopItem>
+) : DiffUtil.Callback(){
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        // благодаря данному методу адаптер вдупляет, что этот тот же самый элемент, просто случилось
+        // какое-то изменение (к примеру, поменялась позиция)
+       return oldList[oldItemPosition].id == newList[newItemPosition].id
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        // оке, благодаря прошлому методу мы поняли, что это тот же элемент, теперь мы должны понять,
+        // есть ли в нем какие-либо изменения, дабы РВ понял, нужно его перерисовывать или нет
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
