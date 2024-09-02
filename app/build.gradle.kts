fa743ba16b53bc3b4a3e65d7a7748aeb72fbe62b
Dmitry <dominicbellucci04@gmail.com>
2024-09-02 19:07:03 +0300

    дадаа

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    id("androidx.navigation.safeargs")
+    id("kotlin-kapt")
 }
 
 android {
